d81088ad33e932d96ad918fec9c54058d0c7eade
Kostas Natsopoulos <natso@natso.gr>
2017-05-22 09:33:14 +0300

    Remove useless object manager property

--- Block/Payment/Redirect.php
+++ Block/Payment/Redirect.php
@@ -6,13 +6,11 @@ class Redirect extends \Magento\Framework\View\Element\Template
 {
     public      $customerSession;
     public      $logger;
-    protected   $_objectManager;
     protected   $_helper;
 
 	public function __construct(
         \Magento\Framework\View\Element\Template\Context    $context,
         \Magento\Customer\Model\Session                     $customerSession,
-        \Magento\Framework\ObjectManagerInterface           $_objectManager,
         \Natso\Piraeus\Helper\Data                          $_helper,
         \Psr\Log\LoggerInterface                            $logger,
         array                                               $data = []
@@ -20,7 +18,6 @@ class Redirect extends \Magento\Framework\View\Element\Template
 	{
         parent::__construct($context, $data);
         $this->customerSession  = $customerSession;
-        $this->_objectManager   = $_objectManager;
         $this->_helper          = $_helper;
         $this->logger           = $logger;
 	}
